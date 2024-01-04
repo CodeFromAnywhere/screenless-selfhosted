b190fd087eaf4bd49aa5b28da122a50e10be9d60
Wijnand Karsens <wijnand@karsens.com>
2024-01-04 09:37:21 +0100

    improvements

--- src/pages/api/screenless.ts
+++ src/pages/api/screenless.ts
@@ -1,23 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   message: string;
-  body?:any
-}
+  body?: any;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  const authorization = req.headers.authorization
-  if (req.method === 'POST' && authorization===process.env.SCREENLESS_SECRET) {
+  const authorization = req.headers.authorization;
+  if (
+    req.method === "POST" &&
+    authorization === process.env.SCREENLESS_SECRET
+  ) {
     // Process a POST request
-    res.status(200).json({ message: 'Stored', body:req.body  })
+    res.status(200).json({ message: "Stored", body: req.body });
   } else {
-    res.status(200).json({ message: 'Please make a post request'  })
- 
+    res.status(200).json({ message: "Please make a post request" });
   }
-
-  
 }
